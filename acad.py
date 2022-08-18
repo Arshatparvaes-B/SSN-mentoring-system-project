628976eec724da21019aa7d51c86b6536c6abe43
narayanan08 <108535185+narayanan08@users.noreply.github.com>
2022-08-18 21:32:39 +0530

    Add files via upload

---
+++ acad.py
@@ -0,0 +1,48 @@
+from main import cat1,cat2
+import ARRAY
+
+def math(arr,n=3):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+def beee(arr,n=4):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+def phy(arr,n=5):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+def pds(arr,n=6):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+def hse(arr,n=7):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+def evs(arr,n=8):
+    marks=ARRAY.Arr()
+    marks.delete()
+    for i in arr:
+        marks.append([i[0],i[1],i[n]])
+    return marks
+
+
+
+
